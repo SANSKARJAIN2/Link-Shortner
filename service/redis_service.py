c213c3e8c0933fbaa8303f15695a92e8f893fca9
sanskarjain02 <sanskar.shanu@gmail.com>
2021-11-29 04:51:45 +0530

    added redis layer

---
+++ service/redis_service.py
@@ -0,0 +1,28 @@
+import redis
+from exceptions.custom_exceptions import UnshortenLinkNF
+from config import host, port
+class Redis_Service():
+    linkToShortDb = redis.Redis()
+    shortToLinkDb = redis.Redis()
+    def set(self,key = "", value = ""):
+        try:
+            self.linkToShortDb.set(key, value)
+            self.shortToLinkDb.set(value, key)
+            return True
+        except Exception as e:
+            print(e)
+            return False
+    def isShortened(self,link):
+        return self.linkToShortDb.exists(link)
+    def canBeRedirected(self,link):
+        return self.shortToLinkDb.exists(link)
+    def getUnshortenLink(self,link):
+        if(self.canBeRedirected(link)):
+            return self.shortToLinkDb.get(link).decode()
+        else:
+            #TODO: add exception here
+            raise UnshortenLinkNF()
+
+    def __init__(self):
+        self.linkToShortDb = redis.Redis(host,port,db = 1)
+        self.shortToLinkDb = redis.Redis(host,port,db = 2)
