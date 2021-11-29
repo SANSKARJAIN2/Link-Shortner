3ae6c7c627a7484140c5b3f4692cc4c69797fcad
sanskarjain02 <sanskar.shanu@gmail.com>
2021-11-29 08:05:58 +0530

    added custom exceptions for link shortening

--- exceptions/custom_exceptions.py
+++ exceptions/custom_exceptions.py
@@ -1,4 +1,23 @@
+class CustomException(Exception):
+    
+    pass
 class UnshortenLinkNF(Exception):
     def __init__(self):
-        message = "Short link not found in DataBase"
-        super().__init__(message)
+        self.message = "Short link not found in DataBase"
+        self.value = 1001
+    def __str__(self):
+        return self.customMessage
+
+class InvalidLink(Exception):
+    def __init__(self):
+        self.message = "Invalid link, please provide a valid link"
+        self.value = 1002
+    def __str__(self):
+        return self.customMessage
+class RedisSetError(Exception):
+
+    def __init__(self,m=""):
+        self.message = "unable to save link in redis db"
+        self.value = 1003
+    def __str__(self):
+        return self.customMessage
